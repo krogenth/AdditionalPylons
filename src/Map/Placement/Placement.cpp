7f58a360ff1d25563c2a4c0aea679d5b77638227
krogenth <kelleydaniel15@gmail.com>
2022-01-22 20:18:45 -0500

    add function documentation, add additional check for valid placement calculation

--- src/Map/Placement/Placement.cpp
+++ src/Map/Placement/Placement.cpp
@@ -6,23 +6,29 @@
 namespace Placement {
 
 	bool isValidPosition(const BWAPI::UnitType& type, const BWAPI::TilePosition& pos, const BWAPI::TilePosition& mapSize) {
+		//	check if position it outside bounds of map
 		if (pos.x < 0 || pos.x > mapSize.x || pos.y < 0 || pos.y > mapSize.y)
 			return false;
+		//	check that it is possible to build at this location
 		return BWAPI::Broodwar->canBuildHere(pos, type);
 	}
 
 	BWAPI::TilePosition getOpenPlacement(const BWAPI::UnitType& type) {
+		//	grab require information, Player starting location and map size
 		BWAPI::TilePosition start = BWAPI::Broodwar->self()->getStartLocation();
 		BWAPI::TilePosition mapSize = BWAPI::TilePosition(BWAPI::Broodwar->mapWidth(), BWAPI::Broodwar->mapHeight());
 
+		//	start visited list and depth queue
 		std::set<BWAPI::TilePosition> visited;
 		std::queue<BWAPI::TilePosition> posQueue({start});
+
+		//	iterate depth queue until a position is found or no more positions exist
 		while (!posQueue.empty()) {
 			auto pos = posQueue.front();
 			visited.emplace(pos);
 			posQueue.pop();
 
-			if (isValidPosition(type, pos, mapSize))
+			if (visited.find(pos) == visited.end() && isValidPosition(type, pos, mapSize))
 				return pos;
 
 			if (((pos.x - 1) >= 0) && (visited.find(BWAPI::TilePosition(pos.x - 1, pos.y)) != visited.end()))
--- src/Map/Placement/Placement.h
+++ src/Map/Placement/Placement.h
@@ -4,6 +4,12 @@
 
 namespace Placement {
 
+	/*
+	Attempts to find the nearest open position to the player starting position on the map
+	@returns
+		@retval BWAPI::TilePositions:Invalid if there was no valid position in the map
+		@retval A BWAPI::TilePosition nearest to the BWAPI::Broodwar->self() starting location
+	*/
 	BWAPI::TilePosition getOpenPlacement(const BWAPI::UnitType& type);
 
 };
