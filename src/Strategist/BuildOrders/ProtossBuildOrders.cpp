9e8ac5c10b3aa9cb7a4564676c3e8ce0d8b47f34
rickrathbun <77806376+rickrathbun@users.noreply.github.com>
2022-02-11 00:34:04 -0500

    fix redefinition error

---
+++ src/Strategist/BuildOrders/ProtossBuildOrders.cpp
@@ -0,0 +1,46 @@
+#include "ProtossBuildOrders.h"
+// Opt for 5 pool on small maps
+// https://liquipedia.net/starcraft/4/5_Pool
+std::initializer_list<std::pair<BWAPI::UnitType, int>> protoss_smallest = {
+    {BWAPI::UnitTypes::Zerg_Drone, 8},
+    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 10},
+    {BWAPI::UnitTypes::Zerg_Drone, 8},
+    {BWAPI::UnitTypes::Zerg_Drone, 10},
+    {BWAPI::UnitTypes::Zerg_Zergling, 12},
+    {BWAPI::UnitTypes::Zerg_Zergling, 14},
+    {BWAPI::UnitTypes::Zerg_Zergling, 16},
+    {BWAPI::UnitTypes::Zerg_Overlord, 18}
+};
+
+// 9 pool on medium maps
+// https://liquipedia.net/starcraft/9_Pool_(vs._Protoss)
+std::initializer_list<std::pair<BWAPI::UnitType, int>> protoss_medium = {
+    {BWAPI::UnitTypes::Zerg_Drone, 8},
+    {BWAPI::UnitTypes::Zerg_Drone, 10},
+    {BWAPI::UnitTypes::Zerg_Drone, 12},
+    {BWAPI::UnitTypes::Zerg_Drone, 14},
+    {BWAPI::UnitTypes::Zerg_Drone, 16},
+    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 18},
+    {BWAPI::UnitTypes::Zerg_Drone, 16},
+    {BWAPI::UnitTypes::Zerg_Overlord, 18},
+    {BWAPI::UnitTypes::Zerg_Drone, 20},
+    {BWAPI::UnitTypes::Zerg_Zergling, 22},
+    {BWAPI::UnitTypes::Zerg_Zergling, 24},
+    {BWAPI::UnitTypes::Zerg_Zergling, 26},
+    {BWAPI::UnitTypes::Zerg_Zergling, 28}
+};
+
+// 12 hatch on large maps
+// https://liquipedia.net/starcraft/12_Hatch_(vs._Protoss)
+std::initializer_list<std::pair<BWAPI::UnitType, int>> protoss_large = {
+    {BWAPI::UnitTypes::Zerg_Drone, 8},
+    {BWAPI::UnitTypes::Zerg_Drone, 10},
+    {BWAPI::UnitTypes::Zerg_Drone, 12},
+    {BWAPI::UnitTypes::Zerg_Drone, 14},
+    {BWAPI::UnitTypes::Zerg_Drone, 16},
+    {BWAPI::UnitTypes::Zerg_Overlord, 18},
+    {BWAPI::UnitTypes::Zerg_Drone, 20},
+    {BWAPI::UnitTypes::Zerg_Drone, 22},
+    {BWAPI::UnitTypes::Zerg_Hatchery, 24},
+    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 22}
+};
--- src/Strategist/BuildOrders/ZergBuildOrders.h
+++ src/Strategist/BuildOrders/ZergBuildOrders.h
@@ -1,53 +1,9 @@
 #pragma once
-// Opt for 4 pool on small maps
-// https://liquipedia.net/starcraft/4/5_Pool
-std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_smallest = {
-    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 8},
-    {BWAPI::UnitTypes::Zerg_Zergling, 6},
-    {BWAPI::UnitTypes::Zerg_Zergling, 8},
-    {BWAPI::UnitTypes::Zerg_Zergling, 10},
-    {BWAPI::UnitTypes::Zerg_Zergling, 12},
-    {BWAPI::UnitTypes::Zerg_Zergling, 14},
-    {BWAPI::UnitTypes::Zerg_Zergling, 16},
-    {BWAPI::UnitTypes::Zerg_Overlord, 18}
-};
+#include<utility>
+#include<BWAPI.h>
 
-// 9 pool on medium maps
-// https://liquipedia.net/starcraft/9_Pool_(vs._Zerg)
-std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_medium = {
-    {BWAPI::UnitTypes::Zerg_Drone, 8},
-    {BWAPI::UnitTypes::Zerg_Drone, 10},
-    {BWAPI::UnitTypes::Zerg_Drone, 12},
-    {BWAPI::UnitTypes::Zerg_Drone, 14},
-    {BWAPI::UnitTypes::Zerg_Drone, 16},
-    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 18},
-    {BWAPI::UnitTypes::Zerg_Drone, 16},
-    {BWAPI::UnitTypes::Zerg_Extractor, 18},
-    {BWAPI::UnitTypes::Zerg_Overlord, 16},
-    {BWAPI::UnitTypes::Zerg_Zergling, 16},
-    {BWAPI::UnitTypes::Zerg_Zergling, 18},
-    {BWAPI::UnitTypes::Zerg_Zergling, 20}
-};
+extern std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_smallest;
+
+extern std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_medium; 
     
-// 12 hatch on large maps
-// https://liquipedia.net/starcraft/12_Hatch_(vs._Zerg)
-std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_large = {
-    {BWAPI::UnitTypes::Zerg_Drone, 8},
-    {BWAPI::UnitTypes::Zerg_Drone, 10},
-    {BWAPI::UnitTypes::Zerg_Drone, 12},
-    {BWAPI::UnitTypes::Zerg_Drone, 14},
-    {BWAPI::UnitTypes::Zerg_Drone, 16},
-    {BWAPI::UnitTypes::Zerg_Overlord, 18},
-    {BWAPI::UnitTypes::Zerg_Drone, 18},
-    {BWAPI::UnitTypes::Zerg_Drone, 20},
-    {BWAPI::UnitTypes::Zerg_Drone, 22},
-    {BWAPI::UnitTypes::Zerg_Hatchery, 24},
-    {BWAPI::UnitTypes::Zerg_Spawning_Pool, 22},
-    {BWAPI::UnitTypes::Zerg_Extractor, 20},
-    {BWAPI::UnitTypes::Zerg_Drone, 18},
-    {BWAPI::UnitTypes::Zerg_Drone, 20},
-    {BWAPI::UnitTypes::Zerg_Drone, 22},
-    {BWAPI::UnitTypes::Zerg_Zergling, 24},
-    {BWAPI::UnitTypes::Zerg_Zergling, 26},
-    {BWAPI::UnitTypes::Zerg_Zergling, 28},
-};
+extern std::initializer_list<std::pair<BWAPI::UnitType, int>> zerg_large;
