1abddd8fbc2c0705d056dac09e8e2cce821db986
Pricea810 <pricea810@strose.edu>
2022-03-24 07:31:54 -0400

    Updated damage calculations

--- src/Strategist/CombatEngine/CombatEngine.cpp
+++ src/Strategist/CombatEngine/CombatEngine.cpp
@@ -9,6 +9,49 @@ std::tuple<double,double> CombatEngine::calcDPS(BWAPI::UnitType troop) {
 	return retVal;
 }
 
+void CombatEngine::resetCombatStats(combatStats stats)
+{
+	stats.numTroops = 0;
+	stats.currHealth = 0;
+	stats.currShield = 0;
+	stats.totalArmor = 0;
+	stats.avgAlt = 0;
+	stats.airDPS = 0;
+	stats.groundDPS = 0;
+	stats.groundNumHits = 0;
+	stats.airNumHits = 0;
+	stats.avgRange = 0;
+}
+
+void CombatEngine::applyHitChance()
+{
+	//account for altitude
+	if ((playerSt.air.avgAlt - enemySt.air.avgAlt) >= 1) {
+		enemySt.air.airDPS *= 0.53125;
+	}
+	else if ((playerSt.air.avgAlt - enemySt.air.avgAlt) <= -1) {
+		playerSt.air.airDPS *= 0.53125;
+	}
+	if ((playerSt.air.avgAlt - enemySt.ground.avgAlt) >= 1) {
+		enemySt.ground.airDPS *= 0.53125;
+	}
+	else if ((playerSt.air.avgAlt - enemySt.ground.avgAlt) <= -1) {
+		playerSt.air.groundDPS *= 0.53125;
+	}
+	if ((playerSt.ground.avgAlt - enemySt.air.avgAlt) >= 1) {
+		enemySt.air.groundDPS *= 0.53125;
+	}
+	else if ((playerSt.ground.avgAlt - enemySt.air.avgAlt) <= -1) {
+		playerSt.ground.airDPS *= 0.53125;
+	}
+	if ((playerSt.ground.avgAlt - enemySt.ground.avgAlt) >= 1) {
+		enemySt.ground.groundDPS *= 0.53125;
+	}
+	else if ((playerSt.ground.avgAlt - enemySt.ground.avgAlt) <= -1) {
+		playerSt.ground.groundDPS *= 0.53125;
+	}
+}
+
 void CombatEngine::setCombatStats(const BWAPI::Position& center, const int radius) {
 	if (center.isValid()) {
 		int topx = center.x - radius;
@@ -20,50 +63,76 @@ void CombatEngine::setCombatStats(const BWAPI::Position& center, const int radiu
 		if (topLeft.isValid() && botRight.isValid()) {
 			std::unordered_map<int, BWAPI::Unit> enemyTroops = enemy.getUnitsByArea(topLeft, botRight);
 			std::unordered_map<int, BWAPI::Unit> myTroops = player.getUnitsByArea(topLeft, botRight);
-			for (const auto& [key, value] : enemyTroops){
+			resetCombatStats(playerSt.air);
+			resetCombatStats(playerSt.ground);
+			resetCombatStats(enemySt.air);
+			resetCombatStats(enemySt.ground);
+			for (const auto& [key, value] : enemyTroops) {
 				BWAPI::UnitType troop = value->getType();
 				auto dpsVals = calcDPS(troop);
 				if (value->isFlying()) {
-					CombatEngine::enemySt.air.numTroops++;
-					CombatEngine::enemySt.air.airDPS += std::get<0>(dpsVals);
-					CombatEngine::enemySt.air.groundDPS += std::get<1>(dpsVals);
-					CombatEngine::enemySt.air.maxShield += troop.maxShields();
-					CombatEngine::enemySt.air.maxHealth += troop.maxHitPoints();
-					CombatEngine::enemySt.air.currHealth += value->getHitPoints();
-					CombatEngine::enemySt.air.currShield += value->getShields();
+					enemySt.air.numTroops++;
+					enemySt.air.airDPS += std::get<0>(dpsVals);
+					enemySt.air.groundDPS += std::get<1>(dpsVals);
+					enemySt.air.currHealth += value->getHitPoints();
+					enemySt.air.currShield += value->getShields();
+					enemySt.air.airNumHits += troop.airWeapon().damageFactor();
+					enemySt.air.groundNumHits += troop.groundWeapon().damageFactor();
+					enemySt.air.totalArmor += troop.armor();
+					enemySt.air.avgAlt += BWAPI::Broodwar->getGroundHeight(value->getTilePosition());
 				}
 				else {
-					CombatEngine::enemySt.ground.numTroops++;
-					CombatEngine::enemySt.ground.airDPS += std::get<0>(dpsVals);
-					CombatEngine::enemySt.ground.groundDPS += std::get<1>(dpsVals);
-					CombatEngine::enemySt.ground.maxShield += troop.maxShields();
-					CombatEngine::enemySt.ground.maxHealth += troop.maxHitPoints();
-					CombatEngine::enemySt.ground.currHealth += value->getHitPoints();
-					CombatEngine::enemySt.ground.currShield += value->getShields();
+					enemySt.ground.numTroops++;
+					enemySt.ground.airDPS += std::get<0>(dpsVals);
+					enemySt.ground.groundDPS += std::get<1>(dpsVals);
+					enemySt.ground.currHealth += value->getHitPoints();
+					enemySt.ground.currShield += value->getShields();
+					enemySt.ground.airNumHits += troop.airWeapon().damageFactor();
+					enemySt.ground.groundNumHits += troop.groundWeapon().damageFactor();
+					enemySt.ground.totalArmor += troop.armor();
+					enemySt.ground.avgAlt += BWAPI::Broodwar->getGroundHeight(value->getTilePosition());
 				}
 			}
+			enemySt.air.avgAlt /= enemySt.air.numTroops;
+			enemySt.ground.avgAlt /= enemySt.ground.numTroops;
 			for (const auto& [key, value] : myTroops) {
 				BWAPI::UnitType troop = value->getType();
 				auto dpsVals = calcDPS(troop);
 				if (value->isFlying()) {
-					CombatEngine::playerSt.air.numTroops++;
-					CombatEngine::playerSt.air.airDPS += std::get<0>(dpsVals);
-					CombatEngine::playerSt.air.groundDPS += std::get<1>(dpsVals);
-					CombatEngine::playerSt.air.maxShield += troop.maxShields();
-					CombatEngine::playerSt.air.maxHealth += troop.maxHitPoints();
-					CombatEngine::playerSt.air.currHealth += value->getHitPoints();
-					CombatEngine::playerSt.air.currShield += value->getShields();
+					playerSt.air.numTroops++;
+					playerSt.air.airDPS += std::get<0>(dpsVals);
+					playerSt.air.groundDPS += std::get<1>(dpsVals);
+					playerSt.air.currHealth += value->getHitPoints();
+					playerSt.air.currShield += value->getShields();
+					playerSt.air.airNumHits += troop.airWeapon().damageFactor();
+					playerSt.air.groundNumHits += troop.groundWeapon().damageFactor();
+					playerSt.air.totalArmor += troop.armor();
+					playerSt.air.avgAlt += BWAPI::Broodwar->getGroundHeight(value->getTilePosition());
 				}
 				else {
-					CombatEngine::playerSt.ground.numTroops++;
-					CombatEngine::playerSt.ground.airDPS += std::get<0>(dpsVals);
-					CombatEngine::playerSt.ground.groundDPS += std::get<1>(dpsVals);
-					CombatEngine::playerSt.ground.maxShield += troop.maxShields();
-					CombatEngine::playerSt.ground.maxHealth += troop.maxHitPoints();
-					CombatEngine::playerSt.ground.currHealth += value->getHitPoints();
-					CombatEngine::playerSt.ground.currShield += value->getShields();
+					playerSt.ground.numTroops++;
+					playerSt.ground.airDPS += std::get<0>(dpsVals);
+					playerSt.ground.groundDPS += std::get<1>(dpsVals);
+					playerSt.ground.currHealth += value->getHitPoints();
+					playerSt.ground.currShield += value->getShields();
+					playerSt.ground.airNumHits += troop.airWeapon().damageFactor();
+					playerSt.ground.groundNumHits += troop.groundWeapon().damageFactor();
+					playerSt.ground.totalArmor += troop.armor();
+					playerSt.ground.avgAlt += BWAPI::Broodwar->getGroundHeight(value->getTilePosition());
 				}
 			}
+			playerSt.air.avgAlt /= playerSt.air.numTroops;
+			playerSt.ground.avgAlt /= playerSt.ground.numTroops;
+			//reduce damage from armor
+			playerSt.air.airDPS -= (playerSt.air.airNumHits * enemySt.air.totalArmor);
+			playerSt.ground.airDPS -= (playerSt.ground.airNumHits * enemySt.air.totalArmor);
+			playerSt.air.groundDPS -= (playerSt.air.groundNumHits * enemySt.ground.totalArmor);
+			playerSt.ground.groundDPS -= (playerSt.ground.groundNumHits * enemySt.ground.totalArmor);
+			enemySt.air.airDPS -= (enemySt.air.airNumHits * playerSt.air.totalArmor);
+			enemySt.ground.airDPS -= (enemySt.ground.airNumHits * playerSt.air.totalArmor);
+			enemySt.air.groundDPS -= (enemySt.air.groundNumHits * playerSt.ground.totalArmor);
+			enemySt.ground.groundDPS -= (enemySt.ground.groundNumHits * playerSt.ground.totalArmor);
+			applyHitChance();
 		}
 		else {
 			throw std::invalid_argument("Area is not valid");
@@ -76,23 +145,87 @@ void CombatEngine::setCombatStats(const BWAPI::Position& center, const int radiu
 
 bool CombatEngine::playerWins(const BWAPI::Position& center, const int radius) {
 	setCombatStats(center, radius);
-	double enemyTotalGroundDPS = CombatEngine::enemySt.ground.groundDPS + CombatEngine::enemySt.air.groundDPS;
-	double enemyTotalAirDPS = CombatEngine::enemySt.ground.airDPS + CombatEngine::enemySt.air.airDPS;
-	double myTotalGroundDPS = CombatEngine::playerSt.ground.groundDPS + CombatEngine::playerSt.air.groundDPS;
-	double myTotalAirDPS = CombatEngine::playerSt.ground.airDPS + CombatEngine::playerSt.air.airDPS;
-	//for now we'll treat the shields as part of the current health bar since we are handling damage for them the same way
-	CombatEngine::playerSt.ground.currHealth += CombatEngine::playerSt.ground.currShield;
-	CombatEngine::playerSt.air.currHealth += CombatEngine::playerSt.air.currShield;
-	CombatEngine::enemySt.ground.currHealth += CombatEngine::enemySt.ground.currShield;
-	CombatEngine::enemySt.air.currHealth += CombatEngine::enemySt.air.currShield;
+	
 	while (!(CombatEngine::playerSt.air.currHealth <= 0) || !(CombatEngine::playerSt.ground.currHealth <= 0)) {
 		if (CombatEngine::enemySt.air.currHealth <= 0 && CombatEngine::enemySt.ground.currHealth <= 0) {
 			return true;
 		}
-		CombatEngine::playerSt.ground.currHealth -= enemyTotalGroundDPS;
-		CombatEngine::playerSt.air.currHealth -= enemyTotalAirDPS;
-		CombatEngine::enemySt.ground.currHealth -= myTotalGroundDPS;
-		CombatEngine::enemySt.air.currHealth -= myTotalAirDPS;
+		//player air troops damage
+		if (playerSt.air.airDPS>playerSt.air.groundDPS && enemySt.air.currHealth>0) {
+			if (enemySt.air.currShield>0) {
+				enemySt.air.currShield -= playerSt.air.airDPS;
+				if (enemySt.air.currShield < 0) { enemySt.air.currHealth += enemySt.air.currShield; }
+			}
+			else {
+				enemySt.air.currHealth -= playerSt.air.airDPS;
+			}
+		}
+		else {
+			if (enemySt.ground.currShield > 0) {
+				enemySt.ground.currShield -= playerSt.air.groundDPS;
+				if (enemySt.ground.currShield < 0) { enemySt.ground.currHealth += enemySt.ground.currShield; }
+			}
+			else {
+				enemySt.ground.currHealth -= playerSt.air.groundDPS;
+			}
+		}
+		//player ground troops damage
+		if (playerSt.ground.airDPS > playerSt.ground.groundDPS && enemySt.air.currHealth > 0) {
+			if (enemySt.air.currShield > 0) {
+				enemySt.air.currShield -= playerSt.ground.airDPS;
+				if (enemySt.air.currShield < 0) { enemySt.air.currHealth += enemySt.air.currShield; }
+			}
+			else {
+				enemySt.air.currHealth -= playerSt.ground.airDPS;
+			}
+		}
+		else {
+			if (enemySt.ground.currShield > 0) {
+				enemySt.ground.currShield -= playerSt.ground.groundDPS;
+				if (enemySt.ground.currShield < 0) { enemySt.ground.currHealth += enemySt.ground.currShield; }
+			}
+			else {
+				enemySt.ground.currHealth -= playerSt.ground.groundDPS;
+			}
+		}
+		//enemy air troops damage
+		if (enemySt.air.airDPS > enemySt.air.groundDPS && playerSt.air.currHealth > 0) {
+			if (playerSt.air.currShield > 0) {
+				playerSt.air.currShield -= enemySt.air.airDPS;
+				if (playerSt.air.currShield < 0) { playerSt.air.currHealth += playerSt.air.currShield; }
+			}
+			else {
+				playerSt.air.currHealth -= enemySt.air.airDPS;
+			}
+		}
+		else {
+			if (playerSt.ground.currShield > 0) {
+				playerSt.ground.currShield -= enemySt.air.groundDPS;
+				if (playerSt.ground.currShield < 0) { playerSt.ground.currHealth += playerSt.ground.currShield; }
+			}
+			else {
+				playerSt.ground.currHealth -= enemySt.air.groundDPS;
+			}
+		}
+		//enemy ground troops damage
+		if (enemySt.ground.airDPS > enemySt.ground.groundDPS && playerSt.air.currHealth > 0) {
+			if (playerSt.air.currShield > 0) {
+				playerSt.air.currShield -= enemySt.ground.airDPS;
+				if (playerSt.air.currShield < 0) { playerSt.air.currHealth += playerSt.air.currShield; }
+			}
+			else {
+				playerSt.air.currHealth -= enemySt.ground.airDPS;
+			}
+		}
+		else {
+			if (playerSt.ground.currShield > 0) {
+				playerSt.ground.currShield -= enemySt.ground.groundDPS;
+				if (playerSt.ground.currShield < 0) { playerSt.ground.currHealth += playerSt.ground.currShield; }
+			}
+			else {
+				playerSt.ground.currHealth -= enemySt.ground.groundDPS;
+			}
+		}
 		
 	}
 	return false;
--- src/Strategist/CombatEngine/CombatEngine.h
+++ src/Strategist/CombatEngine/CombatEngine.h
@@ -4,15 +4,16 @@
 #include "../../Players/Player.h"
 
 struct combatStats {
-	int numTroops=0;
-	double maxHealth=0;
-	double currHealth=0;
-	double maxShield=0;
-	double currShield=0;
-	int avgArmor=0;
-	int avgAlt=0;
-	double airDPS=0;
-	double groundDPS=0;
+	int numTroops = 0;
+	double currHealth = 0;
+	double currShield = 0;
+	int totalArmor = 0;
+	double avgAlt = 0;
+	double airDPS = 0;
+	double groundDPS = 0;
+	int groundNumHits = 0;
+	int airNumHits = 0;
+	double avgRange = 0;
 };
 
 struct playerStats {
@@ -36,7 +37,22 @@ public:
 	*/
 	bool playerWins(const BWAPI::Position& center, const int radius);
 
+	/*
+		Calculates the DPS of a troop
+		@returns
+			@retval std::tuple<double airDPS,double groundDPS>
+	*/
 	std::tuple<double,double> calcDPS(BWAPI::UnitType troop);	
+
+	/*
+		Resets an instance of the combatStats structure to all 0's
+	*/
+	void resetCombatStats(combatStats stats);
+
+	/*
+		Applies the hit chance modifiers to both player and enemy DPS numbers
+	*/
+	void applyHitChance();
 	playerStats playerSt;
 	playerStats enemySt;
 };
